cdc96d1a8d313f4e1a1319ed2db15e45dbbb7584
matiasraisanen <matias.raisanen@gmail.com>
2022-05-01 22:59:38 +0300

    add try excepts

--- AudioPlayer.py
+++ AudioPlayer.py
@@ -5,39 +5,44 @@ import time
 
 class AudioPlayer:
     def __init__(self, base_url, loglevel: str='error', logfile: str='radio.log'):
-
-        ### Logger setup ###
-        if loglevel == 'info':
-            loglevel = logging.INFO
-        elif loglevel == 'debug':
-            loglevel = logging.DEBUG
-        elif loglevel == 'error':
-            loglevel = logging.ERROR
-        # else:
-        #     raise Exception(f'Tried to use invalid loglevel \'{loglevel}\'')
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(loglevel)
-        
-        formatter = logging.Formatter(fmt='[%(asctime)s]-[%(name)s]-[%(levelname)s]: %(message)s', datefmt='%H:%M:%S')
-        
-        fileHandler = logging.FileHandler(logfile)
-        fileHandler.setFormatter(formatter)
-        fileHandler.setLevel(loglevel)
-        self.logger.addHandler(fileHandler)
-        self.logger.info(f'Logging to {logfile}')
+        try:
+            ### Logger setup ###
+            if loglevel == 'info':
+                loglevel = logging.INFO
+            elif loglevel == 'debug':
+                loglevel = logging.DEBUG
+            elif loglevel == 'error':
+                loglevel = logging.ERROR
+            # else:
+            #     raise Exception(f'Tried to use invalid loglevel \'{loglevel}\'')
+            self.logger = logging.getLogger(__name__)
+            self.logger.setLevel(loglevel)
             
-        self.logger.debug(f'Logger setup finished for {__name__} module')
-        ### Logger setup finished ###
+            formatter = logging.Formatter(fmt='[%(asctime)s]-[%(name)s]-[%(levelname)s]: %(message)s', datefmt='%H:%M:%S')
+            
+            fileHandler = logging.FileHandler(logfile)
+            fileHandler.setFormatter(formatter)
+            fileHandler.setLevel(loglevel)
+            self.logger.addHandler(fileHandler)
+            self.logger.info(f'Logging to {logfile}')
+                
+            self.logger.debug(f'Logger setup finished for {__name__} module')
+            ### Logger setup finished ###
 
-        self.instance = Instance('--input-repeat=-1', '-q')
-        self.player=self.instance.media_player_new()
-        self.base_url = base_url
+            self.instance = Instance('--input-repeat=-1', '-q')
+            self.player=self.instance.media_player_new()
+            self.base_url = base_url
+        except Exception as e:
+            self.logger.error(e)
 
     def play(self, station: str='chillsynth'):
         self.logger.debug(f'Press play')
-        self.media=self.instance.media_new(f'{self.base_url}/{station}.m4a')
-        self.player.set_media(self.media)
-        self.player.play()
+        try:
+            self.media=self.instance.media_new(f'{self.base_url}/{station}.m4a')
+            self.player.set_media(self.media)
+            self.player.play()
+        except Exception as e:
+            self.logger.error(e)
 
     def stop(self):
         self.logger.debug(f'Press stop')
@@ -49,9 +54,12 @@ class AudioPlayer:
     def set_volume(self, volume):
         # Volume must be times eleven, so we can reach close to 100% max volume :-D
         # Hey at least it's linear!
-        volume_percent = volume * 11
-        self.logger.debug(f'Set volume to {volume_percent}%')
-        self.player.audio_set_volume(volume_percent)
+        try:
+            volume_percent = volume * 11
+            self.logger.debug(f'Set volume to {volume_percent}%')
+            self.player.audio_set_volume(volume_percent)
+        except Exception as e:
+            self.logger.error(e)
 
 if __name__ == '__main__':
     player = AudioPlayer(loglevel='debug', logfile='radio.log')
